5c6ae4fff740bb2e9a508b7df42490368006b4ec
Tell Me With Data Admin <tell.me.with.data@gmail.com>
2022-06-16 01:07:46 -0500

    Second Part Complete

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Course of FastAPI
 
 1. Fundamentals uses of FastAPI.
+2. Endpoints and Models.
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get("/")
 def home():
     return {"hello": "world"}
+
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
